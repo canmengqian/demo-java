11f6cfaf4a844e3ddf3a6003cec1b1442c2ecd29
zhengzuozhang <2570389155@qq.com>
2023-12-13 10:51:55 +0800

    add: io

---
+++ web/springweb-demo/src/main/java/com/example/springwebdemo/SpringwebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebDemoApplication.class, args);
+    }
+
+}
