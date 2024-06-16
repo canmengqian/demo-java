20521a46a61cc1e02ce4d31b295604635bc739c7
canmengqian <2570389155@qq.com>
2024-06-16 11:50:13 +0800

    init:kotlin

--- basic/src/test/java/demojava/bean/Test.java
+++ basic/src/test/java/demojava/bean/Test.java
@@ -3,9 +3,9 @@ package demojava.bean;
 public class Test {
     record  Person(String name,int age){}
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
       Person person=  new Person ("aa",1);
       person.age ();
       person.name();
-    }
+    }*/
 }
