df0c0d66b91523af27b254a48d5f23d8d8abd8d5
canmengqian <2570389155@qq.com>
2024-06-20 22:19:59 +0800

    init:kotlin

--- basic/src/main/java/org/demo/basic/App.java
+++ basic/src/main/java/org/demo/basic/App.java
@@ -1,13 +1,63 @@
+/*
 package org.demo.basic;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+*/
 /**
  * Hello world!
  *
- */
+ *//*
+
 public class App 
 {
     public static void main( String[] args )
+
     {
-        System.out.println( "Hello World!" );
+        A a = new B();
+        B b = new A();
+
+        List<A> as = new ArrayList<> ();
+        as.add(new A());
+        as.add(new B());
+        as.add(new C());
+        List<B> bs = new ArrayList<> ();
+        bs.add(new B());
+        bs.add(new C());
+
+        as = bs;
+
+        A[] aa = new A[3];
+        aa[0] = new A();
+        aa[1] = new B();
+        aa[2] = new C();
+
+        B[] bb  = new B[3];
+        bb[0] = new B();
+        bb[1] = new C();
+        bb[2] = new C();
+        aa=bb;
+
+        List<? super B> l1 = new LinkedList<>();
+        l1.add(new C());
+        l1.add(new B());
+       l1.add(new A());
+
+        List<? extends A> l2 = new LinkedList();
+        l2.add(new C());
+        l2.add(new B());
+        l2.add(new A());
+
+
+
+    }
+    static class A{
+    }
+   static class B extends A{
+    }
+    static class C extends B{
     }
 }
+*/
